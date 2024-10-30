380d63e917c4ee128e164b19ed41db1093165af3
Dante Vettori <vettori.dante@itismeucci.com>
2024-10-30 19:39:00 +0100

    fade-in v2

--- script/script.js
+++ script/script.js
@@ -1,6 +1,5 @@
 //primi usi observer
 let texts = document.querySelectorAll(".myObsR, .myObsL, .myObsB");
-const spotLi = document.getElementsByClassName("spots");
 //i due dati devono essere obbligatori
 //observer serve per eseguire azioni di obs 
 //ma non deve essere passato
@@ -9,26 +8,6 @@ function actionPerformed(entry, observer){
         entry[0].target.classList.add("fade-in");
         observer.unobserve(entry[0].target)
     }
-}
-
-//crea le frecce
-function createArrow(){
-    let i = document.createElement("i");
-    i.classList.add("bi", "bi-arrow-right");
-    return i;
-}
-
-//aggiunge le frecce
-function addArrow(){
-    for(let i=0; i<spotLi.length; i++){
-        let tmp = spotLi[i].children;
-    for(let x=0; x < tmp.length; x++){
-        if(tmp[x].tagName.toLowerCase() == "li"){
-            tmp[x].appendChild(createArrow());
-        }
-        
-    }
-}
 }
 
 addEventListener("DOMContentLoaded", () => {
